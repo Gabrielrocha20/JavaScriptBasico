f063e68706672a195c160821d4bb417a48a13c2d
Gabriel Rocha <gabrielrocha12.gs@gmail.com>
2022-09-11 15:51:48 -0300

    first commit

---
+++ operadores.js
@@ -0,0 +1,14 @@
+// let primeiroNumero = 20
+// let segundoNumero = 2
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log('Soma =', soma)
+
+const numero = 10
+console.log(numero, typeof numero)
+
+const numeroEmFormatoDeString = String(numero)
+console.log(numeroEmFormatoDeString, typeof numeroEmFormatoDeString)
+
+console.log(parseFloat(1.40))
---
+++ primeiro.js
@@ -0,0 +1,32 @@
+console.log('Hello Word!')
+
+let idade = 26 // Criando a primeira variavel
+
+// Tipos de Variaveis
+idade = 30  // Number
+let numero = 30  // Number
+let altura = 1.77  // Number
+let nome = 'Gabriel'  // String
+let estudando = true  // booleano
+
+// console.log(numero, typeof numero)
+// console.log(altura) 
+// console.log(nome)
+
+// let i = 1
+// while(i <= 5){
+//     if(i % 2 == 1){
+//        console.log('impar') 
+//     }
+//     else{
+//         console.log('par')
+//     }
+    
+//     i += 1
+// }
+
+function soma(x, y) {
+    return x + y
+}
+
+console.log(soma(10, 10))
